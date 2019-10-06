56039d6aa3c518ced7e1ffee09d580f1ad056dd2
Dirk Stichling <mytracks@mytracks4mac.com>
2019-10-06 11:11:32 +0200

    initial checkin

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM cptactionhank/netatalk:latest
+
+COPY afp.conf /etc/afp.conf
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker buildx build --platform linux/arm64 -t mytracks/netatalk:latest_arm64 --push .
+docker buildx build --platform linux/arm/v7 -t mytracks/netatalk:latest_arm_v7 --push .
+docker buildx build --platform linux/amd64 -t mytracks/netatalk:latest --push .
